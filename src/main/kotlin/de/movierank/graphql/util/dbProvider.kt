61466dc0a9133447814ef86634ce290fa61259c4
sorinirimies <mihaiirimies@gmail.com>
2019-02-27 16:04:10 +0100

    use default dispatcher for the coroutines DB transactions

---
+++ src/main/kotlin/de/movierank/graphql/util/dbProvider.kt
@@ -0,0 +1,38 @@
+package de.movierank.graphql.util
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import de.movierank.graphql.model.Movies
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun initExposedDb() {
+    logger().debug("Connecting to DB...")
+    Database.connect(hikari).also {
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(Movies)
+        }
+    }
+}
+
+private val hikari by lazy(LazyThreadSafetyMode.NONE) {
+    HikariDataSource(HikariConfig().apply {
+        driverClassName = "org.h2.Driver"
+        jdbcUrl = "jdbc:h2:mem:movierank"
+        maximumPoolSize = 3
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    })
+}
+
+suspend fun <T> dbQuery(block: () -> T): T = coroutineScope {
+    withContext(Dispatchers.Default) { transaction { block() } }
+}
