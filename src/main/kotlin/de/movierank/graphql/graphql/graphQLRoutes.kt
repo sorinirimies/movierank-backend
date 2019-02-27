a40c4db60d988fd9f05de19231825d5851beb5bd
sorinirimies <mihaiirimies@gmail.com>
2019-02-27 15:56:29 +0100

    import and method signature fixes

---
+++ src/main/kotlin/de/movierank/graphql/graphql/graphQLRoutes.kt
@@ -0,0 +1,43 @@
+package de.movierank.graphql.graphql
+
+import com.github.pgutkowski.kgraphql.schema.Schema
+import com.google.gson.Gson
+import de.movierank.graphql.util.logger
+import io.ktor.application.call
+import io.ktor.request.receive
+import io.ktor.response.respondText
+import io.ktor.routing.Route
+import io.ktor.routing.post
+
+
+data class GraphQLRequest(val query: String = "", val variables: Map<String, Any> = emptyMap())
+
+fun GraphQLErrors.asMap(): Map<String, Map<String, String>> {
+    return mapOf(
+        "errors" to mapOf(
+            "message" to "Caught ${e.javaClass.simpleName}: ${e.message?.replace("\"", "")}"
+        )
+    )
+}
+
+data class GraphQLErrors(val e: Exception)
+
+fun Route.graphql(gson: Gson, schema: Schema) {
+    post("/graphql") {
+        val request = call.receive<GraphQLRequest>()
+
+        val query = request.query
+        logger().info("the graphql query: $query")
+
+        val variables = gson.toJson(request.variables)
+        logger().info("the graphql variables: $variables")
+
+        try {
+            val result = schema.execute(query, variables)
+            call.respondText(result)
+        } catch (e: Exception) {
+            call.respondText(gson.toJson(GraphQLErrors(e).asMap()))
+        }
+    }
+
+}
