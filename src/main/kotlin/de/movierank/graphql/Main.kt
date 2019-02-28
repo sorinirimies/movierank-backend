cbe223bd35a79df61f8a902a348734951d063fbd
sorinirimies <mihaiirimies@gmail.com>
2019-02-28 14:44:44 +0100

    clea

--- src/main/kotlin/de/movierank/graphql/Main.kt
+++ src/main/kotlin/de/movierank/graphql/Main.kt
@@ -1,35 +1,39 @@
 package de.movierank.graphql
 
 import de.movierank.graphql.graphql.graphql
-import de.movierank.graphql.service.MoviesService
-import de.movierank.graphql.service.movies
-import de.movierank.graphql.util.*
+import de.movierank.graphql.util.gsonProvider
+import de.movierank.graphql.util.initExposedDb
+import de.movierank.graphql.util.loadLocalData
+import de.movierank.graphql.util.logger
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
+import io.ktor.http.content.default
+import io.ktor.http.content.static
 import io.ktor.routing.Routing
 import io.ktor.server.netty.EngineMain
-import kotlinx.coroutines.launch
 
 fun Application.module() {
     install(CallLogging)
     install(DefaultHeaders)
-    initExposedDb()
-    install(Routing) {
-        graphql(gsonProvider, movieRankSchema.schema)
-        movies(MoviesService(client))
-    }
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
         }
     }
-    launch {
+    install(Routing) {
+        graphql(gsonProvider)
+        static("/") {
+            default("index.html")
+        }
+    }
+    initExposedDb().also {
         loadLocalData()
     }
+    logger().info("App loaded.")
 }
 
 fun main(args: Array<String>) = EngineMain.main(args)
